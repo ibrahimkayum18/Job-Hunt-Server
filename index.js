034d5c775c519a3ad3ede46a73311e709ca0820e
ibrahimkayum18 <ibrahimkayum017@gmail.com>
2023-11-06 01:05:18 +0600

    filter function working on

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pqcfxjd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -47,6 +47,10 @@ async function run() {
     //update operation
     app.put('/allJobs/:id', async(req, res) => {
         const id = req.params.id;
+        const query = {_id: ObjectId(id)}
+        const body = req.body;
+        const result = await allJobsCollection.findOne(query)
+        res.send(result)
 
     })
 
