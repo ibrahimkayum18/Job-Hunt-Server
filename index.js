4204a2ecc88a799f096b075252f0f20a46f9482c
ibrahimkayum18 <ibrahimkayum017@gmail.com>
2024-01-06 18:52:22 +0600

    editted

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run() {
     app.post('/jwt', async(req, res) => {
       const user = req.body;
 // console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1hr'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'2hr'})
       res.cookie('token',token, {
         httpOnly:true,
         secure: true,
@@ -82,7 +82,7 @@ async function run() {
     })
 
     //get data from data base
-    app.get('/applyed',verifyToken, async(req, res) => {
+    app.get('/applyed', async(req, res) => {
       let query = {}
       // console.log(req.query.email);
         const email = req.query.email;
